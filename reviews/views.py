262e2188729bef36d23e8bb12c6d1bf5a5d5e0a7
Enias Oliveira <eniasoliveira27@gmail.com>
2021-06-30 18:07:42 -0300

    perf: adds cache to reviews endpoint

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,10 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
+
 from django.db.models import Count, Avg
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+
+from silk.profiling.profiler import silk_profile
 
 from recipes.models import Recipe
 from .serializers import ReviewSerializer
@@ -25,3 +30,8 @@ class ReviewViewSet(ReadOnlyModelViewSet):
     )
 
     serializer_class = ReviewSerializer
+
+    @method_decorator(cache_page(600))
+    @silk_profile(name='Reviews List')
+    def list(self, request, *args, **kwargs):
+        return super(ReviewViewSet, self).list(request, *args, **kwargs)
