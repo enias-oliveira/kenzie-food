7c4c5543840855358c6df28b06d929cd0db17ccf
Enias Oliveira <eniasoliveira27@gmail.com>
2021-06-30 18:20:47 -0300

    perf: adds cache to retrieve method in reviews

--- reviews/views.py
+++ reviews/views.py
@@ -35,3 +35,8 @@ class ReviewViewSet(ReadOnlyModelViewSet):
     @silk_profile(name='Reviews List')
     def list(self, request, *args, **kwargs):
         return super(ReviewViewSet, self).list(request, *args, **kwargs)
+
+    @method_decorator(cache_page(600))
+    @silk_profile(name='Reviews Retrieve')
+    def retrieve(self, request, *args, **kwargs):
+        return super(ReviewViewSet, self).retrieve(request, *args, **kwargs)
