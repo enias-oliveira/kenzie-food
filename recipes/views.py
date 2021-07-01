14aab09ea548692eb59b0774c6d5e64f2f4af89a
Enias Oliveira <eniasoliveira27@gmail.com>
2021-06-30 22:32:03 -0300

    perf: adds caching to servings route

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,25 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# from django.utils.decorators import method_decorator
-# from django.views.decorators.cache import cache_page
-
-# from silk.profiling.profiler import silk_profile
+from silk.profiling.profiler import silk_profile
 
 from recipes.models import Recipe
+
 from .serializers import ServingsSerializer
 
 
 class ServingsViewSet(ReadOnlyModelViewSet):
     queryset = Recipe.objects.all().order_by("-recipe_servings")
     serializer_class = ServingsSerializer
+
+    @method_decorator(cache_page(600))
+    @silk_profile(name="Servings List")
+    def list(self, request, *args, **kwargs):
+        return super(ServingsViewSet, self).list(request, *args, **kwargs)
+
+    @method_decorator(cache_page(600))
+    @silk_profile(name="Servings Retrieve")
+    def retrieve(self, request, *args, **kwargs):
+        return super(ServingsViewSet, self).retrieve(request, *args, **kwargs)
